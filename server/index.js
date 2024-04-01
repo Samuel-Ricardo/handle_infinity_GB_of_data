4af1f9592b64de9f81b0e70857703323873755d9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-01 16:41:45 -0300

    [ :sparkles: ] | read and convert data to JSON (app::server)

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ import { stat } from "node:fs/promises";
 import { TransformStream } from "node:stream/web";
 import { Readable, Transform, Writable } from "node:stream";
 import { setTimeout } from "node:timers/promises";
+
 import byteSize from "byte-size";
+import csvtojson from "csvtojson";
+import { title } from "node:process";
 
 const PORT = 3000;
 const headers = {
@@ -25,4 +28,38 @@ createServer(async (request, response) => {
   const { size } = await stat(filename);
 
   console.log(`Processing ${byteSize(size)}`);
+
+  try {
+    response.writeHead(200, headers);
+    const abortController = new AbortController();
+
+    request.once("close", (_) => {
+      console.log("Connection was closed :()", counter);
+      abortController.abort();
+    });
+
+    await Readable.toWeb(createReadStream(filename))
+      .pipeThrough(
+        Transform.toWeb(
+          csvtojson({ headers: ["title", "description", "url_anime"] }),
+        ),
+      )
+      .pipeThrough(
+        new TransformStream({
+          async transform(jsonLine, controller) {
+            const data = JSON.parse(Buffer.from(jsonLine));
+            const mappedData = JSON.stringify({
+              title: data.title,
+              description: data.description,
+              url_anime: data.url_anime,
+            });
+            counter++;
+            controller.enqueue(mappedData.concat("\n"));
+          },
+        }),
+      )
+      .pipeTo(Writable.toWeb(response), { signal: abortController.signal });
+  } catch (error) {
+    console.error(error);
+  }
 });
